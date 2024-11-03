e5815eed7ab20e6c24ba36195e30469ad20d2090
maximopalopoli <mpalopoli@fi.uba.ar>
2024-11-03 07:27:23 -0300

    Add comment

--- server/executables/move_up_command.cpp
+++ server/executables/move_up_command.cpp
@@ -9,5 +9,5 @@ void MoveUpCommand::execute(Game& game) {
     std::cout << "Jugador " << player_id << " se movió hacia arriba\n";
     
     Duck* duck = game.getDuckById(player_id);
-    duck->speed_y --;
+    duck->speed_y --; // Si queremos que salte dos casillas deberíamos cambiar esto
 }
