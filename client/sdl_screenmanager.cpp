634e6d0e4d1413bfb3fa158c077408bf06d585db
napoli-lucia <lucianapoli15@gmail.com>
2024-11-17 15:24:58 -0300

    agrego click en los botones y render

--- client/sdl_screenmanager.cpp
+++ client/sdl_screenmanager.cpp
@@ -42,8 +42,11 @@ void ScreenManager::loadLobbyScreen() {
         SDL_Texture* sdl_texture = texture_handler.loadSimpleTexture(texture.second);
         lobby_textures[texture.first] = sdl_texture;
     }
-    texture_handler.loadFont("04B_16", "04B_30__.TTF", 16);
+    texture_handler.loadFont("04B_16", "04B_30__.TTF", 90);
+    texture_handler.loadFont("8bit", "8bitOperatorPlus8-Regular.ttf", 25);
+
     texture_handler.saveText("04B_16", "Lobby", {255, 255, 255, 255});
+    texture_handler.saveText("8bit", "Match 1 Created", {255, 255, 255, 255});
 }
 
 SDL_Texture* ScreenManager::getTexture(const std::string& name) const {
@@ -54,20 +57,52 @@ void ScreenManager::showLobbyScreen() {
     SDL_RenderCopy(renderer, getTexture("background"), NULL, NULL);
 
     // Titulo Lobby
-    int size_x = 400, size_y = 100;
-    SDL_Rect textRect = {WINDOW_WIDTH/2-size_x/2, 20, size_x, size_y};
+    SDL_Point t_size;
+    SDL_QueryTexture(texture_handler.getText("Lobby"), NULL, NULL, &t_size.x, &t_size.y);
+    SDL_Rect textRect = {WINDOW_WIDTH/2-t_size.x/2, 20, t_size.x, t_size.y};
     SDL_RenderCopy(renderer, texture_handler.getText("Lobby"), NULL, &textRect);
 
     // BOTONES
-    SDL_Rect start_button_rect = {WINDOW_WIDTH/2-BUTTON_W/2, WINDOW_HEIGHT-120, BUTTON_W, BUTTON_H};
+    Button start = {START_MATCH_CODE, WINDOW_WIDTH/2-BUTTON_W/2, WINDOW_HEIGHT-120, BUTTON_W, BUTTON_H};
+    buttons.push_back(start);
+    SDL_Rect start_button_rect = {start.x, start.y, start.w, start.h};
     SDL_RenderCopy(renderer, getTexture("start-button"), NULL, &start_button_rect);
 
-    SDL_Rect new_match_button_rect = {80, size_y+80, BUTTON_W, BUTTON_H};
+    Button new_match = {NEW_MATCH_CODE, 80, t_size.y+80, BUTTON_W, BUTTON_H};
+    buttons.push_back(new_match);
+    SDL_Rect new_match_button_rect = {new_match.x, new_match.y, new_match.w, new_match.h};
     SDL_RenderCopy(renderer, getTexture("new-match-button"), NULL, &new_match_button_rect);
 
-    SDL_Rect join_button_rect = {WINDOW_WIDTH-BUTTON_W-80, size_y+80, BUTTON_W, BUTTON_H};
+    Button join = {LIST_MATCH_AVAILABLE, WINDOW_WIDTH-BUTTON_W-80, t_size.y+80, BUTTON_W, BUTTON_H};
+    buttons.push_back(join);
+    SDL_Rect join_button_rect = {join.x, join.y, join.w, join.h};
     SDL_RenderCopy(renderer, getTexture("join-button"), NULL, &join_button_rect);
 
+    SDL_RenderPresent(renderer);
+}
+
+Button* ScreenManager::getButton(const uint8_t id) {
+    for (auto& button : buttons) {
+        if (button.id == id) {
+            return &button;
+        }
+    }
+    throw std::runtime_error("Button not found");
+}
+
+void ScreenManager::renderNewMatchText() {
+    SDL_Point size;
+    SDL_QueryTexture(texture_handler.getText("Match 1 Created"), NULL, NULL, &size.x, &size.y);
+    SDL_Rect newRect = {90, 280, size.x, size.y};
+    SDL_RenderCopy(renderer, texture_handler.getText("Match 1 Created"), NULL, &newRect);
+    SDL_RenderPresent(renderer);
+}
+
+void ScreenManager::renderAvailableMatches() {
+    Button* join = getButton(LIST_MATCH_AVAILABLE);
+    SDL_Rect rect = {join->x+30, join->y+join->h+30, 30, 30};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawRect(renderer, &rect);
     SDL_RenderPresent(renderer);
 }
 
--- client/sdl_screenmanager.h
+++ client/sdl_screenmanager.h
@@ -2,6 +2,12 @@
 #define SDL_SCREENMANAGER_H
 #include "sdl_texturehandler.h"
 
+struct Button {
+    uint8_t id;
+    int x, y;
+    int w, h;
+};
+
 class ScreenManager {
 public:
     ScreenManager(SDL_Renderer *renderer, TextureHandler &texture_handler);
@@ -10,9 +16,13 @@ public:
     void showLobbyScreen();
     void loadLobbyScreen();
     SDL_Texture* getTexture(const std::string& name) const;
+    void renderNewMatchText();
+    void renderAvailableMatches();
+    Button* getButton(uint8_t id);
 private:
     SDL_Renderer* renderer;
     TextureHandler& texture_handler;
+    std::vector<Button> buttons;
     std::unordered_map<std::string, SDL_Texture*> lobby_textures;
 };
 
