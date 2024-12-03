4f67326f177d284ad5cd591893284d4a077f6558
LetiAab <laab@fi.uba.ar>
2024-12-03 12:19:25 -0300

    borro funciones que no se usan

--- server/guns/laser_rifle.cpp
+++ server/guns/laser_rifle.cpp
@@ -9,7 +9,7 @@ bool LaserRifle::disparar(int position_x, int position_y, char looking, GameMap*
     if (municiones > 0) {
         //la bala debe aparecer fuera del pato, o sino se mata a si mismo
         int laser_position_x = (looking == LOOKING_RIGHT) ? position_x + DUCK_SIZE_X : position_x - BULLET_SIZE_X;
-        int laser_position_y = (is_looking_up) ? position_y - BULLET_SIZE_Y  : position_y -4;
+        int laser_position_y = (is_looking_up) ? position_y - BULLET_SIZE_Y  : position_y+4;
 
         Position laser_pos(laser_position_x, laser_position_y);
         //si donde debe salir la bala hay una pared, no puedo disparar
